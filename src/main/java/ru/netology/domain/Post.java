09e5b6cda4aaa86dfd0c74f33c9248b1cdf8101b
Ivan Kalashnikov <ik.veresk@gmail.com>
2021-08-13 11:32:41 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postId;
+    private String authorImgUrl;
+    private String authorName;
+    private String dateOfPublication;
+    private String postBody;
+    private String postAttachments;
+    private String postLikeButton;
+    private String postLikeCounter;
+    private String postCommentButton;
+    private String postShareButton;
+    private String postViewingCounter;
+    private String postComment;
+    // + getters/setters
+}
